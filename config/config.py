67d7d3b4da45baa52844a30361a1c0e9bc3103b0
KavyaSinghDogra <162047065+KavyaSinghDogra@users.noreply.github.com>
2024-10-20 23:42:02 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,7 @@
+# config/config.py
+
+API_KEY = "d649482362b3f47b6f3d9e65feff577c"  # Replace with your actual API key
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+METRO_CITIES = ["Delhi", "Mumbai", "Chennai", "Bangalore", "Kolkata", "Hyderabad"]
+DB_PATH = "data/weather_data.db"
+INTERVAL = 300  # 5 minutes
